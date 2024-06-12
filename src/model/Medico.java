652e407e8af19351ff43475eeb10ba14317372f5
Vitor <vitordeazambuja@edu.uniube.br>
2024-06-11 23:19:07 -0300

    Adicionada classes Main, Medico e Paciente

---
+++ src/model/Medico.java
@@ -0,0 +1,89 @@
+package model;
+
+public class Medico {
+    private int id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String telefone;
+    private String endereco;
+    private String especialidade;
+    private int numeroIdentificacao;
+
+    public Medico() {}
+
+    public Medico(int id, String nome, String cpf, String email, String telefone, String endereco, String especialidade, int numeroIdentificacao) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.telefone = telefone;
+        this.endereco = endereco;
+        this.especialidade = especialidade;
+        this.numeroIdentificacao = numeroIdentificacao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public int getNumeroIdentificacao() {
+        return numeroIdentificacao;
+    }
+
+    public void setNumeroIdentificacao(int numeroIdentificacao) {
+        this.numeroIdentificacao = numeroIdentificacao;
+    }
+}
---
+++ src/model/Paciente.java
@@ -0,0 +1,100 @@
+package model;
+
+public class Paciente {
+
+    private int id;
+    private String nome;
+    private String cpf;
+    private String dataNascimento;
+    private String sexo;
+    private String email;
+    private String telefone;
+    private String endereco;
+    private int numeroIdentificacao;
+
+    public Paciente() {}
+
+    public Paciente(int id, String nome, String cpf, String dataNascimento,  String sexo, String email, String telefone, String endereco, int numeroIdentificacao) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+        this.sexo = sexo;
+        this.email = email;
+        this.telefone = telefone;
+        this.endereco = endereco;
+        this.numeroIdentificacao = numeroIdentificacao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getNumeroIdentificacao() {
+        return numeroIdentificacao;
+    }
+
+    public void setNumeroIdentificacao(int numeroIdentificacao) {
+        this.numeroIdentificacao = numeroIdentificacao;
+    }
+}
