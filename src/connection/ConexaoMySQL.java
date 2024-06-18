79ff571238972bfda86827f57f08cc2650fcc5ef
Vitor <vitordeazambuja@edu.uniube.br>
2024-06-17 21:19:10 -0300

    Mudada a senha do banco de dados

--- src/connection/ConexaoMySQL.java
+++ src/connection/ConexaoMySQL.java
@@ -8,7 +8,7 @@ public class ConexaoMySQL {
     private static String status = "Não conectou...";
     private static final String URL = "jdbc:mysql://localhost/crudhospital";
     private static final String USER = "root";
-    private static final String PASSWORD = "admin";
+    private static final String PASSWORD = "admin123";
 
     // Método Construtor da Classe
     public ConexaoMySQL() {
